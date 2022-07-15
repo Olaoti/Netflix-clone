f2680ffc062edd6ef380840004e6318f06a1b066
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-07-15 23:02:20 +0100

    netflix-clone

---
+++ src/Row.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import Youtube from "react-youtube";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = "";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+          console.log(url);
+          console.log(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            key={movie.id}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function api() {
+  return (
+    <div>
+920c139c0c206685b9eefd32717d5220
+
+https://api.themoviedb.org/3/movie/550?api_key=920c139c0c206685b9eefd32717d5220
+
+    </div>
+  )
+}
+
+export default api
